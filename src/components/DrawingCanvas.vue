6328691c04a2bb41bcaf0edc8129fc5a701bd5a9
xiej2520 <jackyxie2520@outlook.com>
2023-01-10 01:02:34 -0500

    Automatic resizing for pixi canvas

--- src/components/DrawingCanvas.vue
+++ src/components/DrawingCanvas.vue
@@ -1,8 +1,9 @@
 <template>
-	<canvas
-		id="canvas" ref="canvas"
-		class="min-h-[200px] min-w-[200px] h-full w-full border-blue-600 border-2">
-	</canvas>
+	<div
+		id="canvasDiv" ref="canvasDiv"
+		class="min-h-[200px] min-w-[200px] h-full w-full border-blue-600 border-2"
+	>
+	</div>
 </template>
 
 <script lang="ts">
@@ -12,42 +13,48 @@ export default {
 	mounted() {
 		const pixiStore = usePixiStore();
 
-		let canvas = this.$refs.canvas as HTMLCanvasElement;
-		let ctx = canvas.getContext("2d");
-		
-		let rem = parseInt(getComputedStyle(document.documentElement).fontSize) 
-			* window.devicePixelRatio || 1;
-		let boundingRect = canvas.getBoundingClientRect();
-		let dpr = window.devicePixelRatio || 1;
-		
-		let canvasWidth = boundingRect.width * dpr;
-		let canvasHeight = boundingRect.height * dpr;
-		
+		let canvasDiv = this.$refs.canvasDiv as HTMLDivElement;
+
 		if (pixiStore.app === null) {
 			console.log("created pixiapp")
 			pixiStore.app = new PIXI.Application({
-				width: canvasWidth,
-				height: canvasHeight
+				antialias: true,
+				resizeTo: canvasDiv,
 			});
+			pixiStore.app.ticker.maxFPS = 60;
 		}
 		else {
 			console.log("used existing")
 		}
-		canvas.replaceWith((pixiStore.app.view as HTMLCanvasElement));
-		console.log(canvasWidth, canvasHeight)
+		canvasDiv.appendChild(pixiStore.app.view as HTMLCanvasElement);
 		
 		let g = new PIXI.Graphics();
 		g.lineStyle(0);
-		g.beginFill(0xff00ff, 1);
+		g.beginFill(0x00ff00, 1);
 		g.drawCircle(50, 50, 200);
 		g.endFill();
 		pixiStore.app.stage.addChild(g);
-		console.log(pixiStore.app.stage)
+		let x = 200;
+		let y = 300;
+		pixiStore.app.ticker.add(function () {
+			if (x > 800) {
+				x = 0;
+			}
+			if (y < 0) {
+				y = 645;
+			}
+			x++;
+			y--;
+			g.beginFill(0xff00ff, 1);
+			g.drawCircle(x, y, 20);
+			g.endFill();
+		});
+		pixiStore.app.start();
 	},
 	unmounted() {
 		const pixiStore = usePixiStore();
 		(pixiStore.app?.view as HTMLCanvasElement).remove();
-		pixiStore.app?.destroy();
+		pixiStore.app?.destroy(); // stop memory leak?
 		pixiStore.app = null;
 	}
 }
